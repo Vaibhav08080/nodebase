e656dbf1b908d304769b23f1a4b630bccb01dbd4
Vaibhav08080 <vibhuuu05@gmail.com>
2025-11-05 16:38:10 +0530

    03 : loading data but by trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import prisma from '@/lib/db'
+
+import {trpc, getQueryClient} from '@/trpc/server'
+import { Client } from './client';
+import { get } from 'http';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
 const page = async  () => {
-  const Users = await prisma.user.findMany()
-  return (
-    <div>page users {JSON.stringify(Users)}</div>
-  )
+ const queryClient= getQueryClient()
+ void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+
+ return(
+  <HydrationBoundary state={dehydrate(queryClient)}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Client  />
+    </Suspense>
+  </HydrationBoundary>
+)
+
 }
 
 export default page
