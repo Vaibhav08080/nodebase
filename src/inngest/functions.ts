a6d84657ca504161c306cf9adf54c5cad31df26a
Vaibhav08080 <vibhuuu05@gmail.com>
2025-11-16 21:15:34 +0530

    Sentry added

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,16 +3,26 @@ import prisma from "@/lib/db";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
 import { step } from "inngest";
+import * as Sentry from "@sentry/nextjs";
+
+
 const google = createGoogleGenerativeAI();
 export const execute = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
+  
   async ({ event, step }) => {
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' }) 
     const { steps } = await step.ai.wrap("gemini-generate-text", generateText, {
       model: google("gemini-2.5-flash"),
       system: "You are a helpfull assistant",
       prompt: "what is 2+2",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
-    return steps
+    return steps;
   }
 );
